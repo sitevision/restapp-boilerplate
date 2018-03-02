42581bdffb0c212205abcf9b8979540150263ced
Robin Lagren <robin.lagren@sitevision.se>
2018-03-02 11:57:28 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,90 @@
+var DEV_PROPERTIES = '.dev_properties.json';
+
+module.exports = function(grunt) {
+   grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+
+      compress: {
+         main: {
+            options: {
+               archive: 'dist/<%= pkg.name %>.zip'
+            },
+            expand: true,
+            cwd: 'src/',
+            src: ['**/*']
+         }
+      },
+
+      http_upload: {
+         upload: {
+            options: {
+               url: 'http://<%= username %>:<%= password %>@<%= domain %>/rest-api/1/0/<%= siteName %>/Addon%20Repository/<%= addonName %>/restAppImport<% if (force) { %>?force=true<% } %>',
+               onComplete: function(data) {
+                  console.log('Response: ' + data);
+               }
+            },
+            src: 'dist/<%= pkg.name %>.zip',
+            dest: 'fileupload'
+         }
+      },
+
+      prompt: {
+         setupdev: {
+            options: {
+               questions: [
+                  {
+                     name: 'domain',
+                     type: 'input',
+                     message: 'Website domain (www.example.com)'
+                  },
+                  {
+                     name: 'siteName',
+                     type: 'input',
+                     message: 'Site name'
+                  },
+                  {
+                     name: 'addonName',
+                     type: 'input',
+                     message: 'Addon name'
+                  },
+                  {
+                     name: 'username',
+                     type: 'input',
+                     message: 'Username'
+                  },
+                  {
+                     name: 'password',
+                     type: 'password',
+                     message: 'Password'
+                  }
+               ],
+               then: function(results, done) {
+                  grunt.file.write('.dev_properties.json', JSON.stringify({
+                     domain: results.domain,
+                     siteName: results.siteName,
+                     addonName: results.addonName,
+                     username: results.username,
+                     password: results.password
+                  }));
+               }
+            }
+         }
+      }
+   });
+
+   grunt.loadNpmTasks('grunt-contrib-compress');
+   grunt.loadNpmTasks('grunt-http-upload');
+   grunt.loadNpmTasks('grunt-prompt');
+
+   grunt.registerTask('setup', 'prompt:setupdev');
+   grunt.registerTask('zip', 'compress');
+   grunt.registerTask('deploy', function() {
+      if (grunt.file.exists(DEV_PROPERTIES)) {
+         grunt.config.merge(JSON.parse(grunt.file.read(DEV_PROPERTIES)));
+         grunt.config.set('force', grunt.option('force-deploy'));
+         grunt.task.run(['compress', 'http_upload:upload']);
+      } else {
+         grunt.log.write('Must run setup first');
+      }
+   });
+};
---
+++ README.md
@@ -0,0 +1,11 @@
+# restapp-boilerplate
+Boilerplate code for a simple RESTApp
+## Setup
+* `git clone https://github.com/sitevision/restapp-boilerplate.git`
+* `cd restapp-boilerplate`
+* `npm install`
+* `npm run setup`
+## Building
+* `npm run zip` compress `/src` into `/dist`
+* `npm run deploy` compress `/src` into `/dist` and upload to the addon configured in the setup task
+* `npm run force-deploy` compress `/src` into `/dist` and upload to the addon configured in the setup task. This will overwrite the current RESTApp if it has the same RESTApp-version defined in manifest.json.
